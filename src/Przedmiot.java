88e5918e403314b81f1c9862f1cfcc15c059d170
Student <stundern@student.student>
2024-03-05 14:10:04 +0100

    Powodzenia Fifi

---
+++ src/Przedmiot.java
@@ -0,0 +1,49 @@
+import java.util.Objects;
+
+public class Przedmiot {
+    private String nazwaPrzedmiotu;
+    private int liczbaPunktowECTS;
+
+    public Przedmiot(String nazwaPrzedmiotu) {
+        this.nazwaPrzedmiotu = nazwaPrzedmiotu;
+    }
+    public Przedmiot(){
+
+    }
+
+
+
+    public String getNazwaPrzedmiotu() {
+        return nazwaPrzedmiotu;
+    }
+
+    public void setNazwaPrzedmiotu(String nazwaPrzedmiotu) {
+        this.nazwaPrzedmiotu = nazwaPrzedmiotu;
+    }
+
+    public int getLiczbaPunktowECTS() {
+        return liczbaPunktowECTS;
+    }
+
+    public void setLiczbaPunktowECTS(int liczbaPunktowECTS) {
+        this.liczbaPunktowECTS = liczbaPunktowECTS;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Nazwa Przedmiotu: %s; ilosc punktow ECTS: %d",nazwaPrzedmiotu,liczbaPunktowECTS);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Przedmiot przedmiot = (Przedmiot) o;
+        return liczbaPunktowECTS == przedmiot.liczbaPunktowECTS && Objects.equals(nazwaPrzedmiotu, przedmiot.nazwaPrzedmiotu);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nazwaPrzedmiotu, liczbaPunktowECTS);
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,60 @@
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class Student {
+    private String imie;
+    private String nazwisko;
+    private final int numerIndeksu;
+    private int Rokstudiow;
+
+    public ArrayList<Przedmiot> przedmioty = new ArrayList<>();
+
+    public Student(String imie, String nazwisko, int numerIndeksu, int rokstudiow) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.numerIndeksu = numerIndeksu;
+        Rokstudiow = rokstudiow;
+    }
+
+    public String getImie() {
+        return imie;
+    }
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
+
+    public int getNumerIndeksu() {
+        return numerIndeksu;
+    }
+
+    public int getRokstudiow() {
+        return Rokstudiow;
+    }
+
+    public ArrayList<Przedmiot> getPrzedmioty() {
+        return przedmioty;
+    }
+    @Override
+    public String toString() {
+        return String.format("Imie:%s; Nazwisko: %s; NumerIndeksu: %d; rokstudiow: %d",imie,nazwisko,numerIndeksu,Rokstudiow);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return numerIndeksu == student.numerIndeksu && Rokstudiow == student.Rokstudiow && Objects.equals(imie, student.imie) && Objects.equals(nazwisko, student.nazwisko) && Objects.equals(przedmioty, student.przedmioty);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(imie, nazwisko, numerIndeksu, Rokstudiow, przedmioty);
+    }
+    public void dodaj(Przedmiot przedmiot){
+        przedmioty.add(przedmiot);
+        System.out.println("Dodano: " + przedmiot);
+    }
+}
